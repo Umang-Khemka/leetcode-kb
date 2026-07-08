78fa7faa5efcfb32d429b50cf75c6fc28b989632
Umang Khemka <137717384+Umang-Khemka@users.noreply.github.com>
2026-07-08 15:17:00 +0530

    feat: add solution for 0106 - Construct Binary Tree from Inorder and Postorder Traversal

---
+++ Array/0106 - Construct Binary Tree from Inorder and Postorder Traversal/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* build(int inLeft, int inRight, vector<int>& postorder, unordered_map<int,int>& mp, int &postIndex){
+        if(inLeft > inRight){
+            return nullptr;
+        }
+        TreeNode* root = new TreeNode(postorder[postIndex--]);
+
+        int pos = mp[root->val];
+
+        root->right = build(pos + 1, inRight, postorder, mp, postIndex);
+        root->left  = build(inLeft, pos - 1, postorder, mp, postIndex);
+
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int,int> mp;
+
+        for(int i = 0; i < inorder.size(); i++) {
+            mp[inorder[i]] = i;
+        }
+
+        int postIndex = postorder.size() - 1;
+
+        return build(0, inorder.size() - 1, postorder, mp, postIndex);
+    }
+};
