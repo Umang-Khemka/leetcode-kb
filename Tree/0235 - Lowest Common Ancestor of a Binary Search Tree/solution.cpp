07f963896de8829233cfed253506a402c8d5ee01
Umang Khemka <137717384+Umang-Khemka@users.noreply.github.com>
2026-07-09 09:29:11 +0530

    feat: add solution for 0235 - Lowest Common Ancestor of a Binary Search Tree

---
+++ Tree/0235 - Lowest Common Ancestor of a Binary Search Tree/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* dfs(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root == nullptr){
+            return nullptr;
+        }
+        if(root->val > p->val && root->val > q->val){
+            return dfs(root->left,p,q);
+        }
+        if(root->val < p->val && root->val < q->val) {
+            return dfs(root->right,p,q);
+        }
+        return root;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return dfs(root,p,q);
+    }
+};
