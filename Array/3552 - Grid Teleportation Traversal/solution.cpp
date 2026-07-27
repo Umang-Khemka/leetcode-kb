11e17109430b9296a5eb61b335ce627626c89859
Umang Khemka <137717384+Umang-Khemka@users.noreply.github.com>
2026-07-27 23:29:02 +0530

    refactor: update solution for 3552 - Grid Teleportation Traversal

--- Array/3552 - Grid Teleportation Traversal/solution.cpp
+++ Array/3552 - Grid Teleportation Traversal/solution.cpp
@@ -8,44 +8,55 @@ public:
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (matrix[i][j] >= 'A' && matrix[i][j] <= 'Z')
-                    mp[matrix[i][j]].push_back({i, j});
+                char ch = matrix[i][j];
+                if (ch >= 'A' && ch <= 'Z')
+                    mp[ch].push_back({i, j});
             }
         }
 
         vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        deque<pair<int, int>> dq;
 
-        dq.push_front({0, 0});
+        priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>
+        > pq;
+
         dist[0][0] = 0;
+        pq.push({0, {0, 0}});
 
         int dr[] = {-1, 0, 1, 0};
         int dc[] = {0, 1, 0, -1};
 
-        while (!dq.empty()) {
-            auto [r, c] = dq.front();
-            dq.pop_front();
+        while (!pq.empty()) {
+            auto [moves, cell] = pq.top();
+            pq.pop();
 
-            int d = dist[r][c];
+            auto [row, col] = cell;
 
-            if (r == n - 1 && c == m - 1)
-                return d;
+            if (moves > dist[row][col])
+                continue;
 
-            char ch = matrix[r][c];
+            if (row == n - 1 && col == m - 1)
+                return moves;
 
+            char ch = matrix[row][col];
+
+            // Teleport (cost = 0)
             if (ch >= 'A' && ch <= 'Z' && !mp[ch].empty()) {
                 for (auto [x, y] : mp[ch]) {
-                    if (d < dist[x][y]) {
-                        dist[x][y] = d;
-                        dq.push_front({x, y});
+                    if (moves < dist[x][y]) {
+                        dist[x][y] = moves;
+                        pq.push({moves, {x, y}});
                     }
                 }
                 mp[ch].clear();
             }
 
+            // Walk (cost = 1)
             for (int k = 0; k < 4; k++) {
-                int nr = r + dr[k];
-                int nc = c + dc[k];
+                int nr = row + dr[k];
+                int nc = col + dc[k];
 
                 if (nr < 0 || nr >= n || nc < 0 || nc >= m)
                     continue;
@@ -53,9 +64,9 @@ public:
                 if (matrix[nr][nc] == '#')
                     continue;
 
-                if (d + 1 < dist[nr][nc]) {
-                    dist[nr][nc] = d + 1;
-                    dq.push_back({nr, nc});
+                if (moves + 1 < dist[nr][nc]) {
+                    dist[nr][nc] = moves + 1;
+                    pq.push({moves + 1, {nr, nc}});
                 }
             }
         }
