74e1bb69e85d8e9359cf79d5ae462ca4c35aa00a
Umang Khemka <137717384+Umang-Khemka@users.noreply.github.com>
2026-07-27 11:10:08 +0530

    feat: add solution for 1464 - Maximum Product of Two Elements in an Array

---
+++ Array/1464 - Maximum Product of Two Elements in an Array/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int fmax = INT_MIN;
+        int smax = INT_MIN;
+
+        for(int i=0;i<n;i++){
+            if(nums[i] >= fmax){
+                smax = fmax;
+                fmax = nums[i];
+            }
+            else if(nums[i] > smax){
+                smax = nums[i];
+            }
+        }
+        return (fmax - 1) * (smax - 1);
+    }
+};
