3a9233b6b574064400ebb60982d6e3ac444becb5
Umang Khemka <137717384+Umang-Khemka@users.noreply.github.com>
2026-07-30 00:44:19 +0530

    feat: add solution for 3438 - Find Valid Pair of Adjacent Digits in String

---
+++ String/3438 - Find Valid Pair of Adjacent Digits in String/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string findValidPair(string s) {
+        vector<int> freq(10, 0);
+
+        for (char c : s) {
+            freq[c - '0']++;
+        }
+
+        for (int i = 0; i < s.size() - 1; i++) {
+            int a = s[i] - '0';
+            int b = s[i + 1] - '0';
+
+            if (a != b && freq[a] == a && freq[b] == b) {
+                return s.substr(i, 2);
+            }
+        }
+
+        return "";
+    }
+};
